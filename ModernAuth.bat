2bb996bacd36c6129c8865811b0781e6e6359c97
gmisservice <103799703+gmisservice@users.noreply.github.com>
2022-10-19 16:30:44 -0400

    Update ModernAuth.bat
    update to silent install and remove after finish

--- ModernAuth.bat
+++ ModernAuth.bat
@@ -1,5 +1,5 @@
-REG ADD HKEY_CURRENT_USER\Software\Microsoft\Office\15.0\Common\Identity /v EnableADAL /t REG_DWORD /d 1 /s
-REG ADD HKEY_CURRENT_USER\Software\Microsoft\Office\15.0\Common\Identity /v Version /t REG_DWORD /d 1 /s
-REG ADD HKEY_CURRENT_USER\Software\Microsoft\Exchange /v AlwaysUseMSOAuthForAutoDiscover /t REG_DWORD /d 1 /s
-REG ADD HKEY_CURRENT_USER\Software\Microsoft\Office\15.0\Common /s
-del /F C:\programdata\Microsoft\Windows\Start Menu\Programs\Startup\ModernAuth.bat
+REG ADD HKEY_CURRENT_USER\Software\Microsoft\Office\15.0\Common\Identity /v EnableADAL /t REG_DWORD /d 1 /f
+REG ADD HKEY_CURRENT_USER\Software\Microsoft\Office\15.0\Common\Identity /v Version /t REG_DWORD /d 1 /f
+REG ADD HKEY_CURRENT_USER\Software\Microsoft\Exchange /v AlwaysUseMSOAuthForAutoDiscover /t REG_DWORD /d 1 /f
+REG ADD HKEY_CURRENT_USER\Software\Microsoft\Office\15.0\Common /f
+del %0
